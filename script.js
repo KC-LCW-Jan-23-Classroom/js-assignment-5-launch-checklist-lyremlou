b110d11911642b7951ef695edacb10b72162b972
Mary Sanders <marysanders@Marys-MBP.lan>
2023-04-18 16:18:07 -0500

    Get input references and values via DOM in submit event listener

--- script.js
+++ script.js
@@ -13,4 +13,18 @@ window.addEventListener("load", function() {
        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
    })
    
+   let form = document.querySelector("form");
+   form.addEventListener("submit", function(event){
+    let pilotInput = document.querySelector("input[name=pilotName]");
+    let pilot = pilotInput.value;
+    
+    let copilotInput = document.querySelector("input[name=copilotName]");
+    let copilot = copilotInput.value;
+
+    let fuelLevelInput = document.querySelector("input[name=fuelLevel]");
+    let fuelLevel = Number(fuelLevelInput.value);
+
+    let cargoLevelInput = document.querySelector("input[name=cargoMass");
+    let cargoLevel = Number(cargoLevelInput.value); 
+   })
 });
